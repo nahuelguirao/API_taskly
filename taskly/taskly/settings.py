2fb1eb09d264997c79c41a84d85de97e0bf67c3d
Nahuel Guirao <nahuelguirao123@gmail.com>
2024-01-31 12:37:17 -0300

    Prueba 7

--- taskly/taskly/settings.py
+++ taskly/taskly/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'users',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
